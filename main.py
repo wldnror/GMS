7b452f8f2e180ce1676c13261a9aaad9f9841fb7
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-07 09:49:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from analog_ui import AnalogUI
 
 if __name__ == "__main__":
     root = Tk()
+
+    # 전체 화면 설정
+    root.attributes("-fullscreen", True)
+
+    # ESC 키를 눌러 전체 화면 모드를 종료할 수 있도록 이벤트를 설정합니다.
+    def exit_fullscreen(event):
+        root.attributes("-fullscreen", False)
+
+    root.bind("<Escape>", exit_fullscreen)
+
     modbus_boxes = 5  # 원하는 Modbus TCP 상자 수를 설정하세요.
     analog_boxes = 6  # 원하는 4~20mA 상자 수를 설정하세요.
 
