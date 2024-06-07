4ee9edb049c5957ab303586896b49cb562f500a8
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-07 10:10:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-from tkinter import Tk, Frame, Label
+from tkinter import Tk
+from modbus_ui import ModbusUI
+from analog_ui import AnalogUI
 import signal
 import sys
-
-class ModbusUI(Frame):
-    def __init__(self, parent, boxes):
-        super().__init__(parent)
-        self.pack(expand=True, fill='both')
-        self.clients = {}
-        for i in range(boxes):
-            label = Label(self, text=f'Modbus Box {i+1}')
-            label.place(relx=0.5, rely=0.1*(i+1), anchor='center')
-
-class AnalogUI(Frame):
-    def __init__(self, parent, boxes):
-        super().__init__(parent)
-        self.pack(expand=True, fill='both')
-        for i in range(boxes):
-            label = Label(self, text=f'Analog Box {i+1}')
-            label.place(relx=0.5, rely=0.1*(i+1+5), anchor='center')  # Adjust y-position as needed
 
 if __name__ == "__main__":
     root = Tk()
 
     # 전체 화면 설정
     root.attributes("-fullscreen", True)
+
+    # 중앙 정렬 설정
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
 
     # ESC 키를 눌러 전체 화면 모드를 종료할 수 있도록 이벤트를 설정합니다.
     def exit_fullscreen(event):
