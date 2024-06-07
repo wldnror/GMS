6a225408fa1456fa7c41b135817d87c91d99b99e
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-07 09:51:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import Tk
 from modbus_ui import ModbusUI
 from analog_ui import AnalogUI
+import signal
+import sys
 
 if __name__ == "__main__":
     root = Tk()
@@ -13,6 +15,14 @@ if __name__ == "__main__":
         root.attributes("-fullscreen", False)
 
     root.bind("<Escape>", exit_fullscreen)
+
+    # 정상 종료를 위한 핸들러 추가
+    def signal_handler(sig, frame):
+        print("Exiting gracefully...")
+        root.destroy()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
 
     modbus_boxes = 5  # 원하는 Modbus TCP 상자 수를 설정하세요.
     analog_boxes = 6  # 원하는 4~20mA 상자 수를 설정하세요.
