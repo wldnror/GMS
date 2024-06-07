4161eb8bd751a44d4413bb7c024c9360cffd44f3
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-07 10:28:09 +0900

    Update analog_ui.py

--- analog_ui.py
+++ analog_ui.py
@@ -78,14 +78,14 @@ class AnalogUI:
         box_canvas.create_text(148, 175, text="FUT", fill="#cccccc", anchor="n")
 
         # 상자 세그먼트 아래에 "가스명" 글자 추가
-        box_canvas.create_text(129, 105, text="ORG", font=("Helvetica", 20, "bold"), fill="#cccccc", anchor="center")
+        box_canvas.create_text(129, 105, text="ORG", font=("Helvetica", 18, "bold"), fill="#cccccc", anchor="center")
 
         # 상자 맨 아래에 "GDS SMS" 글자 추가
         box_canvas.create_text(87, 295, text="GMS-1000", font=("Helvetica", 20, "bold"), fill="#cccccc",
                                anchor="center")
 
         # 상자 맨 아래에 "GDS ENGINEERING CO.,LTD" 글자 추가
-        box_canvas.create_text(87, 328, text="GDS ENGINEERING CO.,LTD", font=("Helvetica", 10, "bold"), fill="#cccccc",
+        box_canvas.create_text(87, 328, text="GDS ENGINEERING CO.,LTD", font=("Helvetica", 8, "bold"), fill="#cccccc",
                                anchor="center")
 
         # 4~20mA 상자는 bar 관련 UI 요소를 추가하지 않음
