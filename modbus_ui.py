757a37ddcb8d5abf2d8a9f2cabdffe532bc20d98
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-07 12:35:57 +0900

    Update modbus_ui.py

--- modbus_ui.py
+++ modbus_ui.py
@@ -68,7 +68,8 @@ class ModbusUI:
         entry.grid(row=0, column=0, padx=(0, 5))  # 입력 필드 배치
         self.entries.append(entry)
 
-        action_button = Button(frame, image=self.connect_image, command=lambda i=index: self.toggle_connection(i), width=20, height=20,
+        action_button = Button(frame, image=self.connect_image, command=lambda i=index: self.toggle_connection(i),
+                               width=40, height=40,  # 가로 크기를 늘림
                                bd=0, highlightthickness=0, borderwidth=0, relief='flat')
         action_button.grid(row=0, column=1, padx=(0, 5))  # 버튼 배치
         self.action_buttons.append(action_button)
