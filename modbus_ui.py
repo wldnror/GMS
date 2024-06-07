7cb5333853481458b5002eba3d2e78af1617db8e
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-07 12:22:33 +0900

    Update modbus_ui.py

--- modbus_ui.py
+++ modbus_ui.py
@@ -46,11 +46,11 @@ class ModbusUI:
 
         # 이미지 로드 및 변환 (비율 유지, 크기 조절)
         self.connect_image = Image.open(connect_image_path)
-        self.connect_image.thumbnail((20, 50), Image.LANCZOS)
+        self.connect_image.thumbnail((20, 20), Image.LANCZOS)
         self.connect_image = ImageTk.PhotoImage(self.connect_image)
         
         self.disconnect_image = Image.open(disconnect_image_path)
-        self.disconnect_image.thumbnail((20, 50), Image.LANCZOS)
+        self.disconnect_image.thumbnail((20, 20), Image.LANCZOS)
         self.disconnect_image = ImageTk.PhotoImage(self.disconnect_image)
 
         for _ in range(num_boxes):
