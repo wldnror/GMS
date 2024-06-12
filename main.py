6c4272047759eefff15369ea1f785f8771a3d7e3
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-12 15:53:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from tkinter import Tk, Frame, Button, Menu, Toplevel, Label, Entry
+from tkinter import Tk, Frame, Button, Toplevel, Label, Entry, messagebox
+import random
+import time
 from modbus_ui import ModbusUI
 from analog_ui import AnalogUI
 import signal
@@ -8,6 +10,8 @@ import os
 
 # 글로벌 변수로 설정 창을 참조합니다.
 settings_window = None
+attempt_count = 0
+lock_time = 0
 
 # 설정 페이지를 여는 함수
 def show_settings():
@@ -69,6 +73,12 @@ def restart_application():
 
 # 비밀번호 입력 창을 표시하는 함수
 def show_password_prompt():
+    global attempt_count, lock_time
+
+    if time.time() < lock_time:
+        messagebox.showerror("잠금", "비밀번호 입력 시도가 5회 초과되었습니다. 30초 후에 다시 시도하십시오.")
+        return
+
     password_window = Toplevel(root)
     password_window.title("비밀번호 입력")
     password_window.attributes("-topmost", True)
@@ -77,13 +87,50 @@ def show_password_prompt():
     password_entry = Entry(password_window, show="*", font=("Arial", 12))
     password_entry.pack(pady=5)
 
+    def create_keypad():
+        frame = Frame(password_window)
+        frame.pack()
+
+        buttons = [
+            '1', '2', '3',
+            '4', '5', '6',
+            '7', '8', '9',
+            '0', 'CLR', 'DEL'
+        ]
+
+        rows = 4
+        cols = 3
+        for i, button in enumerate(buttons):
+            b = Button(frame, text=button, width=5, height=2,
+                       command=lambda b=button: on_button_click(b, password_entry))
+            b.grid(row=i // cols, column=i % cols, padx=5, pady=5)
+
+    def on_button_click(char, entry):
+        if char == 'DEL':
+            current_text = entry.get()
+            entry.delete(0, tk.END)
+            entry.insert(0, current_text[:-1])
+        elif char == 'CLR':
+            entry.delete(0, tk.END)
+        else:
+            entry.insert(tk.END, char)
+
     def check_password():
-        if password_entry.get() == "your_password":  # 여기에 원하는 비밀번호를 입력하세요
+        nonlocal attempt_count, lock_time
+        if password_entry.get() == "00700":  # 비밀번호 설정
             password_window.destroy()
             show_settings()
         else:
-            Label(password_window, text="비밀번호가 틀렸습니다.", font=("Arial", 12), fg="red").pack(pady=5)
+            attempt_count += 1
+            if attempt_count >= 5:
+                lock_time = time.time() + 30  # 30초 잠금
+                attempt_count = 0
+                password_window.destroy()
+                messagebox.showerror("잠금", "비밀번호 입력 시도가 5회 초과되었습니다. 30초 후에 다시 시도하십시오.")
+            else:
+                Label(password_window, text="비밀번호가 틀렸습니다.", font=("Arial", 12), fg="red").pack(pady=5)
 
+    create_keypad()
     Button(password_window, text="확인", command=check_password).pack(pady=5)
 
 if __name__ == "__main__":
