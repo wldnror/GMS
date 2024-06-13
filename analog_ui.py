b7968ae2f8f4b0d97b36a9173b857fe179da6e3c
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-13 12:33:17 +0900

    Update analog_ui.py

--- analog_ui.py
+++ analog_ui.py
@@ -1,4 +1,9 @@
+import os
+import time
 from tkinter import Frame, Canvas, StringVar
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import mplcursors
 from common import SEGMENTS, create_segment_display, show_history_graph
 
 class AnalogUI:
@@ -6,19 +11,22 @@ class AnalogUI:
         self.root = root
 
         self.box_states = []
-        self.histories = [[] for _ in range(num_boxes)]  # 히스토리 저장을 위한 리스트 초기화
-        self.graph_windows = [None for _ in range(num_boxes)]  # 그래프 윈도우 저장을 위한 리스트 초기화
+        self.histories = [[] for _ in range(num_boxes)]
+        self.graph_windows = [None for _ in range(num_boxes)]
 
         self.box_frame = Frame(self.root)
-        self.box_frame.grid(row=0, column=0, padx=40, pady=40)  # padding 증가
+        self.box_frame.grid(row=0, column=0, padx=40, pady=40)
 
-        self.row_frames = []  # 각 행의 프레임을 저장할 리스트
-        self.box_frames = []  # UI 상자를 저장할 리스트
+        self.row_frames = []
+        self.box_frames = []
+        self.history_dir = "analog_history_logs"  # 로그 파일을 저장할 디렉토리
+
+        if not os.path.exists(self.history_dir):
+            os.makedirs(self.history_dir)
 
         for _ in range(num_boxes):
             self.create_analog_box()
 
-        # 모든 동그라미를 꺼는 초기화
         for i in range(num_boxes):
             self.update_circle_state([False, False, False, False], box_index=i)
 
@@ -29,71 +37,54 @@ class AnalogUI:
 
         if col == 0:
             row_frame = Frame(self.box_frame)
-            row_frame.grid(row=row, column=0)  # grid로 변경
+            row_frame.grid(row=row, column=0)
             self.row_frames.append(row_frame)
         else:
             row_frame = self.row_frames[-1]
 
         box_frame = Frame(row_frame)
-        box_frame.grid(row=0, column=col, padx=20, pady=20)  # padding 증가
+        box_frame.grid(row=0, column=col, padx=20, pady=20)
 
-        box_canvas = Canvas(box_frame, width=200, height=400, highlightthickness=4, highlightbackground="#000000",
-                            highlightcolor="#000000")
+        box_canvas = Canvas(box_frame, width=200, height=400, highlightthickness=4, highlightbackground="#000000", highlightcolor="#000000")
         box_canvas.pack()
 
         box_canvas.create_rectangle(0, 0, 210, 250, fill='grey', outline='grey', tags='border')
         box_canvas.create_rectangle(0, 250, 210, 410, fill='black', outline='grey', tags='border')
 
-        create_segment_display(box_canvas)  # 세그먼트 디스플레이 생성
+        create_segment_display(box_canvas)
         self.box_states.append({
             "blink_state": False,
             "blinking_error": False,
             "previous_value_40011": None,
-            "previous_segment_display": None,  # 이전 세그먼트 값 저장
-            "last_history_time": None,  # 마지막 히스토리 기록 시간
-            "last_history_value": None  # 마지막 히스토리 기록 값
+            "previous_segment_display": None,
+            "last_history_time": None,
+            "last_history_value": None
         })
-        self.update_segment_display("    ", box_canvas, box_index=i)  # 초기화시 빈 상태로 설정
+        self.update_segment_display("    ", box_canvas, box_index=i)
 
-        # 동그라미 상태를 저장할 리스트
         circle_items = []
 
-        # Draw small circles in the desired positions (moved to gray section)
-        # Left vertical row under the segment display
-        circle_items.append(
-            box_canvas.create_oval(133, 200, 123, 190))  # Red circle 1
+        circle_items.append(box_canvas.create_oval(133, 200, 123, 190))
         box_canvas.create_text(95, 220, text="AL1", fill="#cccccc", anchor="e")
 
-        circle_items.append(
-            box_canvas.create_oval(77, 200, 87, 190))  # Red circle 2
+        circle_items.append(box_canvas.create_oval(77, 200, 87, 190))
         box_canvas.create_text(140, 220, text="AL2", fill="#cccccc", anchor="e")
 
-        circle_items.append(
-            box_canvas.create_oval(30, 200, 40, 190))  # Green circle 1
+        circle_items.append(box_canvas.create_oval(30, 200, 40, 190))
         box_canvas.create_text(35, 220, text="PWR", fill="#cccccc", anchor="center")
 
-        # Right horizontal row under the segment display
-        circle_items.append(
-            box_canvas.create_oval(171, 200, 181, 190))  # Yellow circle 1
+        circle_items.append(box_canvas.create_oval(171, 200, 181, 190))
         box_canvas.create_text(175, 213, text="FUT", fill="#cccccc", anchor="n")
 
-
-        # 상자 세그먼트 아래에 "가스명" 글자 추가
         box_canvas.create_text(129, 105, text="ORG", font=("Helvetica", 18, "bold"), fill="#cccccc", anchor="center")
 
-        # 상자 맨 아래에 "GDS SMS" 글자 추가
-        box_canvas.create_text(107, 360, text="GMS-1000", font=("Helvetica", 22, "bold"), fill="#cccccc",
-                               anchor="center")
+        box_canvas.create_text(107, 360, text="GMS-1000", font=("Helvetica", 22, "bold"), fill="#cccccc", anchor="center")
 
-        # 상자 맨 아래에 "GDS ENGINEERING CO.,LTD" 글자 추가
-        box_canvas.create_text(107, 395, text="GDS ENGINEERING CO.,LTD", font=("Helvetica", 9, "bold"), fill="#cccccc",
-                               anchor="center")
+        box_canvas.create_text(107, 395, text="GDS ENGINEERING CO.,LTD", font=("Helvetica", 9, "bold"), fill="#cccccc", anchor="center")
 
-        # 4~20mA 상자는 bar 관련 UI 요소를 추가하지 않음
         self.box_frames.append((box_frame, box_canvas, circle_items, None, None, None))
 
-        # 세그먼트 클릭 시 히스토리를 그래프로 보여주는 이벤트 추가
-        box_canvas.segment_canvas.bind("<Button-1>", lambda event, i=i: show_history_graph(self.root, i, self.histories, self.graph_windows))
+        box_canvas.segment_canvas.bind("<Button-1>", lambda event, i=i: self.show_history_graph(i))
 
     def update_circle_state(self, states, box_index=0):
         _, box_canvas, circle_items, _, _, _ = self.box_frames[box_index]
@@ -107,52 +98,92 @@ class AnalogUI:
             color = colors_on[i] if state else colors_off[i]
             box_canvas.itemconfig(circle_items[i], fill=color, outline=color)
 
-        if states[0]:  # Red top-left
+        if states[0]:
             outline_color = outline_colors[0]
-        elif states[1]:  # Red top-right
+        elif states[1]:
             outline_color = outline_colors[1]
-        elif states[3]:  # Yellow bottom-right
+        elif states[3]:
             outline_color = outline_colors[3]
-        else:  # Default grey outline
+        else:
             outline_color = outline_color_off
 
-        # 박스 테두리 업데이트
         box_canvas.config(highlightbackground=outline_color)
 
     def update_segment_display(self, value, box_canvas, blink=False, box_index=0):
-        value = value.zfill(4)  # Ensure the value is 4 characters long, padded with zeros if necessary
+        value = value.zfill(4)
         leading_zero = True
         blink_state = self.box_states[box_index]["blink_state"]
         previous_segment_display = self.box_states[box_index]["previous_segment_display"]
 
-        if value != previous_segment_display:  # 값이 변했을 때만 기록
+        if value != previous_segment_display:
             self.record_history(box_index, value)
             self.box_states[box_index]["previous_segment_display"] = value
 
         for i, digit in enumerate(value):
             if leading_zero and digit == '0' and i < 3:
-                # 앞의 세 자릿수가 0이면 회색으로 설정
                 segments = SEGMENTS[' ']
             else:
                 segments = SEGMENTS[digit]
                 leading_zero = False
 
             if blink and blink_state:
-                segments = SEGMENTS[' ']  # 깜빡임 상태에서는 모든 세그먼트를 끕니다.
+                segments = SEGMENTS[' ']
 
             for j, state in enumerate(segments):
                 color = '#fc0c0c' if state == '1' else '#424242'
                 box_canvas.segment_canvas.itemconfig(f'segment_{i}_{chr(97 + j)}', fill=color)
 
-        self.box_states[box_index]["blink_state"] = not blink_state  # 깜빡임 상태 토글
+        self.box_states[box_index]["blink_state"] = not blink_state
 
     def record_history(self, box_index, value):
-        if value.strip():  # 값이 공백이 아닌 경우에만 기록
-            last_history_value = self.box_states[box_index]["last_history_value"]
-            if value != last_history_value:
-                timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-                last_value = self.box_states[box_index].get("last_value_40005", 0)
-                self.histories[box_index].append((timestamp, value, last_value))
-                self.box_states[box_index]["last_history_value"] = value
-                if len(self.histories[box_index]) > 100:  # 최대 기록 수를 제한
-                    self.histories[box_index].pop(0)
+        if value.strip():
+            timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+            log_line = f"{timestamp},{value}\n"
+            log_file = os.path.join(self.history_dir, f"box_{box_index}.log")
+
+            with open(log_file, 'a') as file:
+                file.write(log_line)
+
+    def show_history_graph(self, box_index):
+        if hasattr(self, 'history_frame') and self.history_frame.winfo_exists():
+            self.history_frame.destroy()
+
+        self.history_frame = Frame(self.root, bg='white', bd=2, relief='solid')
+        self.history_frame.place(relx=0.5, rely=0.5, anchor='center', width=1200, height=800)
+
+        self.overlay = Frame(self.root, bg='', width=self.root.winfo_screenwidth(), height=self.root.winfo_screenheight())
+        self.overlay.place(x=0, y=0)
+        self.overlay.lower(self.history_frame)
+        self.overlay.bind("<Button-1>", self.hide_history)
+
+        figure = plt.Figure(figsize=(12, 8), dpi=100)
+        ax = figure.add_subplot(111)
+
+        log_file = os.path.join(self.history_dir, f"box_{box_index}.log")
+
+        times = []
+        values = []
+        if os.path.exists(log_file):
+            with open(log_file, 'r') as file:
+                for line in file:
+                    timestamp, value = line.strip().split(',')
+                    times.append(timestamp)
+                    values.append(value)
+
+        ax.plot(times, values, marker='o')
+        ax.set_title('History')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Value')
+        figure.autofmt_xdate()
+
+        canvas = FigureCanvasTkAgg(figure, master=self.history_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
+
+        mplcursors.cursor(ax)
+
+    def hide_history(self, event=None):
+        if hasattr(self, 'history_frame') and self.history_frame.winfo_exists():
+            self.history_frame.destroy()
+        if hasattr(self, 'overlay') and self.overlay.winfo_exists():
+            self.overlay.destroy()
