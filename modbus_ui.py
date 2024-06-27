e96c1e248a904676363529e1c429c5bc6f5a0585
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-27 09:28:06 +0900

    Update modbus_ui.py

--- modbus_ui.py
+++ modbus_ui.py
@@ -32,8 +32,9 @@ class ModbusUI:
     }
 
 
-    def __init__(self, root, num_boxes, gas_types):
+    def __init__(self, root, num_boxes, gas_types, alarm_callback):
         self.root = root
+        self.alarm_callback = alarm_callback  # 알람 콜백 추가
         self.virtual_keyboard = VirtualKeyboard(root)
         self.ip_vars = [StringVar() for _ in range(num_boxes)]  # IP 변수 초기화
         self.entries = []
@@ -219,14 +220,12 @@ class ModbusUI:
             color = colors_on[i] if state else colors_off[i]
             box_canvas.itemconfig(circle_items[i], fill=color, outline=color)
 
-        if states[0]:
-            outline_color = outline_colors[0]
-        elif states[1]:
-            outline_color = outline_colors[1]
-        elif states[3]:
-            outline_color = outline_colors[3]
+        if states[0] or states[1]:
+            outline_color = outline_colors[0 if states[0] else 1]
+            self.alarm_callback(True)  # 알람 활성화
         else:
             outline_color = outline_color_off
+            self.alarm_callback(False)  # 알람 비활성화
 
         box_canvas.config(highlightbackground=outline_color)
 
