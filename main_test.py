915d872fd31d29de2686ebb43746022de1cb3fbb
wldnror <72863449+wldnror@users.noreply.github.com>
2024-07-04 12:25:07 +0900

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,11 +1,8 @@
 import tkinter as tk
-from tkinter import messagebox
 from threading import Thread
 import numpy as np
 from smbus2 import SMBus
 import time
-import csv
-import os
 import joblib
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -19,7 +16,6 @@ DEVICE_ADDRESS = 0x54
 bus = SMBus(BUS_NUMBER)
 
 time_steps = 60  # 전역 변수로 선언
-measuring = False  # 데이터 수집 중인지 여부
 current_values = []  # 전역 변수로 초기화
 clf = joblib.load('gas_classifier.pkl')
 
@@ -80,88 +76,6 @@ def print_and_predict_sensor_data():
                         result.set("IPA입니다")
         time.sleep(1)
 
-# 데이터 수집 함수
-def collect_data(filename, label, samples=100, time_steps=60):
-    global measuring
-    data_list = []
-    collected_samples = 0
-
-    if os.path.exists(filename):
-        with open(filename, 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                if int(row[0]) == label:
-                    collected_samples += 1
-
-    for i in range(collected_samples, samples):
-        sample_data = []
-        while True:
-            initial_data = read_sensor_data()
-            if initial_data is not None and initial_data >= 250:
-                break
-            progress.set(f"수집 대기 중: 가스 농도 {initial_data} ppm")
-            time.sleep(1)
-            
-        for j in range(time_steps):
-            data = read_sensor_data()
-            if data is not None:
-                sample_data.append(data)
-                if len(current_values) >= time_steps:
-                    current_values.pop(0)
-                current_values.append(data)
-                progress.set(f"수집 중: 샘플 {i+1}/{samples}, 데이터 포인트 {j+1}/{time_steps}, 현재 값: {data} ppm")
-            else:
-                print(f"데이터 포인트 읽기 실패: 샘플 {i+1}, 포인트 {j+1}")
-                progress.set(f"수집 실패: 샘플 {i+1}/{samples}, 데이터 포인트 {j+1}/{time_steps}")
-                break
-            time.sleep(1)  # 1초 간격으로 데이터 수집
-        if len(sample_data) == time_steps:
-            data_list.append([label] + sample_data)
-            collected_samples += 1
-            progress.set(f"수집 중: {collected_samples}/{samples} 샘플 완료")
-            print(f"{collected_samples}/{samples} 샘플 수집 완료")
-            if not messagebox.askokcancel("다음 샘플로 이동", f"다음 샘플로 이동하시겠습니까?\n마지막 값: {sample_data[-1]} ppm"):
-                break
-        else:
-            print(f"{collected_samples}/{samples} 샘플 수집 실패")
-    
-    if data_list:
-        with open(filename, 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerows(data_list)
-    
-    messagebox.showinfo("완료", f"{samples}개의 샘플 수집 완료!")
-    progress.set("수집 완료")
-    measuring = False
-    start_button.config(state=tk.NORMAL)
-
-# 데이터 수집 시작 함수
-def start_collection():
-    global measuring
-    if measuring:
-        return
-    measuring = True
-    start_button.config(state=tk.DISABLED)
-    
-    global time_steps
-    gas_type = gas_type_var.get()
-    concentration = concentration_var.get()
-    time_steps = 60
-    if gas_type and concentration:
-        filename = f"{gas_type.lower()}_data.csv"
-        label_map = {
-            "에탄올": 0,
-            "IPA": 1
-        }
-        label = label_map[gas_type]
-        progress.set("수집 시작")
-        collection_thread = Thread(target=collect_data, args=(filename, label))
-        collection_thread.start()
-    else:
-        messagebox.showwarning("입력 오류", "가스 종류와 농도를 선택하세요.")
-        measuring = False
-        start_button.config(state=tk.NORMAL)
-
 # 실시간 그래프 업데이트 함수
 def update_graph(frame):
     if len(current_values) > 0:
@@ -173,41 +87,18 @@ def update_graph(frame):
 
 # GUI 생성
 root = tk.Tk()
-root.title("데이터 수집기")
-
-# 가스 종류 선택 라벨 및 옵션 메뉴
-tk.Label(root, text="가스 종류:").grid(row=0, column=0)
-gas_type_var = tk.StringVar(root)
-gas_type_options = ["에탄올", "IPA"]
-gas_type_menu = tk.OptionMenu(root, gas_type_var, *gas_type_options)
-gas_type_menu.grid(row=0, column=1)
-
-# 농도 선택 라벨 및 옵션 메뉴
-tk.Label(root, text="농도:").grid(row=1, column=0)
-concentration_var = tk.StringVar(root)
-concentration_options = ["100", "50", "25"]
-concentration_menu = tk.OptionMenu(root, concentration_var, *concentration_options)
-concentration_menu.grid(row=1, column=1)
-
-# 데이터 수집 시작 버튼
-start_button = tk.Button(root, text="수집 시작", command=start_collection)
-start_button.grid(row=2, columnspan=2)
-
-# 진행 상황 라벨
-progress = tk.StringVar()
-progress.set("대기 중")
-tk.Label(root, textvariable=progress).grid(row=3, columnspan=2)
+root.title("가스 감지기")
 
 # 실시간 예측 결과 라벨
 result = tk.StringVar()
 result.set("결과 대기 중")
-tk.Label(root, textvariable=result, font=("Helvetica", 16)).grid(row=4, columnspan=2)
+tk.Label(root, textvariable=result, font=("Helvetica", 16)).grid(row=0, columnspan=2)
 
 # 실시간 그래프 표시
 fig, ax = plt.subplots()
 line, = ax.plot([], [], lw=2)
 canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.get_tk_widget().grid(row=5, columnspan=2)
+canvas.get_tk_widget().grid(row=1, columnspan=2)
 ani = FuncAnimation(fig, update_graph, interval=1000, cache_frame_data=False)
 
 # 실시간 센서 데이터 출력 및 예측 스레드 시작
