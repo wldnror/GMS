35f72d0c36545db6bd85f88a4eb5470673a88637
wldnror <72863449+wldnror@users.noreply.github.com>
2024-07-12 15:19:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -152,30 +152,13 @@ def change_branch():
 
     Button(branch_window, text="브랜치 변경", command=switch_branch).pack(pady=10)
 
-def alarm_blink():
-    red_duration = 500  # 빨간색 상태에서 머무는 시간 (밀리초)
-    off_duration = 500  # 기본 배경색 상태에서 머무는 시간 (밀리초)
-
-    def toggle_color():
-        if alarm_active:
-            current_color = root.cget("background")
-            new_color = "red" if current_color != "red" else default_background
-            root.config(background=new_color)
-            root.after(red_duration if new_color == "red" else off_duration, toggle_color)
-        else:
-            root.config(background=default_background)
-            root.after_cancel(toggle_color)
-
-    toggle_color()
-
 def set_alarm_status(active):
     global alarm_active, alarm_blinking
     if alarm_active != active:
         alarm_active = active
         print(f"Alarm status set to: {alarm_active}")  # 상태 출력
         if alarm_active and not alarm_blinking:
-            alarm_blinking = True
-            alarm_blink()
+            alarm_blinking = False
             play_alarm_sound()  # 알람 소리 재생
         elif not alarm_active and alarm_blinking:
             alarm_blinking = False
