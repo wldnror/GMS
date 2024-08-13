17b9e4c6649418ad05d50715c564aa82d8e42b20
wldnror <72863449+wldnror@users.noreply.github.com>
2024-08-13 15:54:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -241,25 +241,20 @@ if __name__ == "__main__":
     column_index = 0
     max_columns = 6  # 한 줄에 최대 6개 상자 배치
 
-    # Modbus 항목을 한 줄에 배치
-    for i, box in enumerate(modbus_boxes):
-        modbus_ui.box_frame.grid(row=row_index, column=column_index, padx=10, pady=10)
-        column_index += 1
+    # Modbus와 Analog 항목을 한 줄에 배치
+    for i in range(max(len(modbus_boxes), len(analog_boxes))):
+        if i < len(modbus_boxes):
+            modbus_ui.box_frame.grid(row=row_index, column=column_index, padx=10, pady=10)
+            column_index += 1
+
+        if i < len(analog_boxes):
+            analog_ui.box_frame.grid(row=row_index, column=column_index, padx=10, pady=10)
+            column_index += 1
 
         # 한 줄에 6개를 초과하면 다음 줄로 이동
         if column_index >= max_columns:
             column_index = 0
             row_index += 1
-
-    # Analog 항목을 동일한 줄에 이어서 배치
-    for i, box in enumerate(analog_boxes):
-        analog_ui.box_frame.grid(row=row_index, column=column_index, padx=10, pady=10)
-        column_index += 1
-
-    # 한 줄에 6개를 초과하면 다음 줄로 이동
-    if column_index >= max_columns:
-        column_index = 0
-        row_index += 1
 
     settings_button = tk.Button(root, text="⚙", command=lambda: prompt_new_password() if not admin_password else show_password_prompt(show_settings), font=("Arial", 20))
     def on_enter(event):
