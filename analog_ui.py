82730f8d24e85fb17e994c0cac951fdbf1915d9a
wldnror <72863449+wldnror@users.noreply.github.com>
2024-08-14 10:49:40 +0900

    Update analog_ui.py

--- analog_ui.py
+++ analog_ui.py
@@ -84,7 +84,7 @@ class AnalogUI:
         box_frame = Frame(row_frame)
         box_frame.grid(row=0, column=col, padx=20, pady=20)
 
-        box_canvas = Canvas(box_frame, width=200, height=400, highlightthickness=4, highlightbackground="#000000", highlightcolor="#000000")
+        box_canvas = Canvas(box_frame, width=200, height=400, highlightthickness=4, highlightbackground="#000000", highlightcolor="#000000", bg='white')
         box_canvas.pack()
 
         box_canvas.create_rectangle(0, 0, 210, 250, fill='grey', outline='grey', tags='border')
