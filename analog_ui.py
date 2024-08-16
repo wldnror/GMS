e62e391846a0df2ea14060bea8faea8e3ca8e620
wldnror <72863449+wldnror@users.noreply.github.com>
2024-08-16 13:38:17 +0900

    Update analog_ui.py

--- analog_ui.py
+++ analog_ui.py
@@ -334,9 +334,8 @@ class AnalogUI:
                 milliamp = current * 1000
                 
                 
-                
                 # mA 교정: 2mA를 더해줍니다.
-                milliamp += 2.0
+                milliamp += 0.2
                 
                 values.append(milliamp)
 
