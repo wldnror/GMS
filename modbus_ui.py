53d7b3c02da2076190aec44c80982e655712a3ba
wldnror <72863449+wldnror@users.noreply.github.com>
2024-08-22 13:46:55 +0900

    Update modbus_ui.py

--- modbus_ui.py
+++ modbus_ui.py
@@ -142,9 +142,8 @@ class ModbusUI:
         box_canvas = Canvas(box_frame, width=int(250 * SCALE_FACTOR), height=int(450 * SCALE_FACTOR), highlightthickness=int(4 * SCALE_FACTOR), highlightbackground="#000000", highlightcolor="#000000")
         box_canvas.pack()
 
-        box_canvas.create_rectangle(0, 0, int(260 * SCALE_FACTOR), int(300 * SCALE_FACTOR), fill='grey', outline='grey', tags='border')
-        box_canvas.create_rectangle(0, int(250 * SCALE_FACTOR), int(260 * SCALE_FACTOR), int(460 * SCALE_FACTOR), fill='black', outline='grey', tags='border')
-
+        box_canvas.create_rectangle(0, 0, 160, 200, fill='grey', outline='grey', tags='border')  # 사각형 크기 조정
+        box_canvas.create_rectangle(0, 200, 160, 310, fill='black', outline='grey', tags='border')
         create_segment_display(box_canvas)
         self.box_states.append({
             "blink_state": False,
