0fd2431fe98b0e4e8fd8c484252111a943e07753
wldnror <72863449+wldnror@users.noreply.github.com>
2024-08-22 12:39:53 +0900

    Update modbus_ui.py

--- modbus_ui.py
+++ modbus_ui.py
@@ -136,7 +136,7 @@ class ModbusUI:
         box_frame = Frame(row_frame)
         box_frame.grid(row=0, column=col, padx=20, pady=20, sticky="w")
 
-        box_canvas = Canvas(box_frame, width=200, height=400, highlightthickness=4, highlightbackground="#000000", highlightcolor="#000000")
+        box_canvas = Canvas(box_frame, width=250, height=450, highlightthickness=4, highlightbackground="#000000", highlightcolor="#000000")
         box_canvas.pack()
 
         box_canvas.create_rectangle(0, 0, 210, 250, fill='grey', outline='grey', tags='border')
