9b7b95c31e1d5fced8af003fe83457efe153e3d2
wldnror <72863449+wldnror@users.noreply.github.com>
2024-08-22 13:50:50 +0900

    Update modbus_ui.py

--- modbus_ui.py
+++ modbus_ui.py
@@ -142,8 +142,8 @@ class ModbusUI:
         box_canvas = Canvas(box_frame, width=int(250 * SCALE_FACTOR), height=int(450 * SCALE_FACTOR), highlightthickness=int(4 * SCALE_FACTOR), highlightbackground="#000000", highlightcolor="#000000")
         box_canvas.pack()
 
-        box_canvas.create_rectangle(0, 0, int(260 * SCALE_FACTOR), int(300 * SCALE_FACTOR), fill='grey', outline='grey', tags='border')
-        box_canvas.create_rectangle(0, int(250 * SCALE_FACTOR), int(260 * SCALE_FACTOR), int(400 * SCALE_FACTOR), fill='black', outline='grey', tags='border')
+        box_canvas.create_rectangle(0, 0, int(260 * SCALE_FACTOR), int(200 * SCALE_FACTOR), fill='grey', outline='grey', tags='border')
+        box_canvas.create_rectangle(0, int(250 * SCALE_FACTOR), int(260 * SCALE_FACTOR), int(450 * SCALE_FACTOR), fill='black', outline='grey', tags='border')
 
         create_segment_display(box_canvas)
         self.box_states.append({
