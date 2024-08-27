2a72308c4ce431aa0de51160e8f08ae3708acf1a
wldnror <72863449+wldnror@users.noreply.github.com>
2024-08-27 15:06:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ if __name__ == "__main__":
         clock_label.place(relx=0.5, rely=0.1, anchor='n')
 
         date_label = tk.Label(root, font=("Helvetica", 29), fg="white", bg="black", anchor='center', padx=5, pady=5)
-        date_label.place(relx=0.5, rely=0.18, anchor='n')
+        date_label.place(relx=0.5, rely=0.25, anchor='n')
 
         def update_clock():
             now = datetime.datetime.now()
