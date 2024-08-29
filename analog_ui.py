edf7dc49342ef6ecf9de40b504dac6ea0fd6f6c2
wldnror <72863449+wldnror@users.noreply.github.com>
2024-08-29 15:58:02 +0900

    Update analog_ui.py

--- analog_ui.py
+++ analog_ui.py
@@ -223,12 +223,20 @@ class AnalogUI:
                 return  # 모든 자릿수 업데이트가 완료된 경우
 
             digit = value[index]
+            # .을 켜는 로직 추가
+            # HMDS 가스이고, full_scale이 3000이면, 각 자리의 점을 켭니다.
+            if gas_type == "HMDS" and full_scale == 3000:
+                segments = SEGMENTS[digit
+                # 마지막 자리는 점을 켜지 않습니다.
+                if index < 3:
+                    segments = segments[:-1] + '1'  # 마지막 비트 (점) 켜기
 
-            if leading_zero and digit == '0' and index < 3:
-                segments = SEGMENTS[' ']
             else:
-                segments = SEGMENTS[digit]
-                leading_zero = False
+                if leading_zero and digit == '0' and index < 3:
+                    segments = SEGMENTS[' ']
+                else:
+                    segments = SEGMENTS[digit]
+                    leading_zero = False
 
             if blink and self.box_states[box_index]["blink_state"]:
                 segments = SEGMENTS[' ']
