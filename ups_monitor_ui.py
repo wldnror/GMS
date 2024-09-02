16a0f02706eed95800c254adf44555b75d1f2801
wldnror <72863449+wldnror@users.noreply.github.com>
2024-09-02 15:16:01 +0900

    Update ups_monitor_ui.py

--- ups_monitor_ui.py
+++ ups_monitor_ui.py
@@ -51,7 +51,7 @@ class UPSMonitorUI:
 
         # UPS 및 제조사 정보
         text_y_position = int(295 * SCALE_FACTOR)  # 동일한 y 좌표 사용
-        box_canvas.create_text(int(80 * SCALE_FACTOR), text_y_position, text="UPS Monitor", font=("Helvetica", int(16 * SCALE_FACTOR), "bold"), fill="#333333", anchor="center")
+        box_canvas.create_text(int(80 * SCALE_FACTOR), text_y_position, text="UPS Monitor", font=("Helvetica", int(13 * SCALE_FACTOR), "bold"), fill="#333333", anchor="center")
         box_canvas.create_text(int(80 * SCALE_FACTOR), text_y_position + int(15 * SCALE_FACTOR), text="GDS ENGINEERING CO.,LTD", font=("Helvetica", int(7 * SCALE_FACTOR), "bold"), fill="#666666", anchor="center")
 
         self.box_frames.append((box_frame, box_canvas))
