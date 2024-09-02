9d2abce946e1a662e3a3e8314a53b869a354c881
wldnror <72863449+wldnror@users.noreply.github.com>
2024-09-02 13:53:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -289,7 +289,7 @@ if __name__ == "__main__":
         raise TypeError("analog_boxes should be a list, got {}".format(type(analog_boxes)))
 
     main_frame = tk.Frame(root)
-    main_frame.grid(row=0, column=0 sticky='w')
+    main_frame.grid(row=0, column=0, sticky='w')
 
     # main.py 내에서 modbus_ui 초기화 부분 수정
     modbus_ui = ModbusUI(main_frame, len(modbus_boxes), settings["modbus_gas_types"], set_alarm_status)
