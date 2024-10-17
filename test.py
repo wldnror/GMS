f5951fa77022f0aa7862da566e09f6e67f8002f0
용굴이 <72863449+wldnror@users.noreply.github.com>
2024-10-17 13:46:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-import Adafruit_ADS1x15
-
-adc = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)  # busnum=1 추가
-GAIN = 1  # 게인 설정
-
-# 채널 0에서 값 읽기
-value = adc.read_adc(0, gain=GAIN)
-print(f'Channel 0: {value}')
+value_ch1 = adc.read_adc(1, gain=GAIN)
+value_ch2 = adc.read_adc(2, gain=GAIN)
+value_ch3 = adc.read_adc(3, gain=GAIN)
+print(f'Channel 1: {value_ch1}')
+print(f'Channel 2: {value_ch2}')
+print(f'Channel 3: {value_ch3}')
