6f3c9ec3b08864dc6be52c85828e15e417da6e69
용굴이 <72863449+wldnror@users.noreply.github.com>
2024-10-17 14:16:44 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-import smbus
-import time
+from smbus2 import SMBus
 
 # I2C 버스 및 ADS1115 주소
-bus = smbus.SMBus(1)
+bus = SMBus(1)
 address = 0x48
 
 # ADS1115 설정 레지스터 (0x01)에 데이터를 쓰기
@@ -10,7 +9,6 @@ config = [0xC2, 0x83]  # 적절한 설정값
 bus.write_i2c_block_data(address, 0x01, config)
 
 # 컨버전 레지스터 (0x00)에서 데이터를 읽기
-time.sleep(0.5)  # 변환 대기 시간
 data = bus.read_i2c_block_data(address, 0x00, 2)
 
 # 데이터 변환
