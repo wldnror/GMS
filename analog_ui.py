5979914367f12b66d7a02589346c10126f099519
용굴이 <72863449+wldnror@users.noreply.github.com>
2024-10-22 15:15:27 +0900

    Update analog_ui.py

--- analog_ui.py
+++ analog_ui.py
@@ -11,6 +11,9 @@ import tkinter as tk
 
 from common import SEGMENTS, create_segment_display, SCALE
 
+# i2c-1 버스 사용
+i2c_bus_number = 1
+
 # 전역 변수로 설정
 GAIN = 2 / 3
 SCALE_FACTOR = 1.65  # 20% 키우기
@@ -290,9 +293,7 @@ class AnalogUI:
     def async_write_log(self, log_file, log_line):
         with open(log_file, 'a') as file:
             file.write(log_line)
-    
-    # i2c-1 버스 사용
-    i2c_bus_number = 1
+
     
     async def read_adc_data(self):
         adc_addresses = [0x48, 0x4A, 0x4B]
