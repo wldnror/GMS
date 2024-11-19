088e36401b585d60b21c93ad78535545b71997e1
용굴이 <72863449+wldnror@users.noreply.github.com>
2024-11-19 15:31:38 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import Adafruit_ADS1x15
 
-GAIN = 2/3
+GAIN = 1
 adc = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
 
-for channel in range(4):
-    value = adc.read_adc(channel, gain=GAIN)
-    voltage = value * 6.144 / 32767
-    current = voltage / 250
-    milliamp = current * 1000
-    print(f"Channel {channel}: {milliamp:.2f} mA")
+channel = 0  # A1 채널만 읽기
+value = adc.read_adc(channel, gain=GAIN)
+voltage = value * 4.096 / 32767
+current = voltage / 250
+milliamp = current * 1000
+print(f"Channel {channel}: {milliamp:.2f} mA")
