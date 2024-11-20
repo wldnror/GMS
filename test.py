621cbf5b45132fb0c74d39e1943c234163ed3b4e
용굴이 <72863449+wldnror@users.noreply.github.com>
2024-11-20 10:40:58 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import Adafruit_ADS1x15
 
 GAIN = 2/3
-adc = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
+adc = Adafruit_ADS1x15.ADS1115(address=0x4A, busnum=1)
+ # 0x4A, 0x4B
 
 for channel in range(4):
     value = adc.read_adc(channel, gain=GAIN)
