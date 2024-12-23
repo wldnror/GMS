72ce8f908754adddc72db1971feefcaeba2809ad
용굴이 <72863449+wldnror@users.noreply.github.com>
2024-12-23 09:05:16 +0900

    Update modbus_ui.py

--- modbus_ui.py
+++ modbus_ui.py
@@ -101,8 +101,8 @@ class ModbusUI:
             bg="#2e2e2e",
             fg="white",
             insertbackground="white",
-            font=("Helvetica", int(10.2 * SCALE_FACTOR)  # 기존 폰트 크기 유지
-            justify='center')  # 텍스트 중앙 정렬 추가
+            font=("Helvetica", int(10.2 * SCALE_FACTOR)),  # 기존 폰트 크기 유지
+            justify='center'  # 텍스트 중앙 정렬 추가
         )
         entry.pack(padx=2, pady=3)
 
