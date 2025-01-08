e5970f5b0d5626b8a2c85371dbcca3b4dd99ecd4
용굴이 <72863449+wldnror@users.noreply.github.com>
2025-01-08 14:10:37 +0900

    Update analog_ui.py

--- analog_ui.py
+++ analog_ui.py
@@ -66,7 +66,7 @@ class AnalogUI:
         inner_frame.pack(padx=int(1), pady=int(1))
 
         box_canvas = Canvas(inner_frame, width=int(150 * SCALE_FACTOR), height=int(300 * SCALE_FACTOR),
-                            highlightthickness=int(3 * SCALE_FACTOR),
+                            highlightthickness=int(0 * SCALE_FACTOR),
                             highlightbackground="#000000", highlightcolor="#000000", bg='white')
         box_canvas.pack()
 
